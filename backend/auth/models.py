ed79c4487cc57b74f78687558c9604f236d394cb
yar-kik <einstein16.04@gmail.com>
2021-04-27 19:23:51 +0300

    separated application for auth

---
+++ backend/auth/models.py
@@ -0,0 +1,23 @@
+from werkzeug.security import generate_password_hash, check_password_hash
+from utils import db
+
+
+class User(db.Model):
+    """Class for user entity in database"""
+    __tablname__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(64), unique=True, index=True)
+    username = db.Column(db.String(64), nullable=False, unique=True, index=True)
+    password_hash = db.Column(db.String(128), nullable=False)
+
+    @property
+    def password(self) -> None:
+        raise AttributeError("password is not a readable attribute")
+
+    @password.setter
+    def password(self, password: str) -> None:
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password: str) -> bool:
+        return check_password_hash(self.password_hash, password)
